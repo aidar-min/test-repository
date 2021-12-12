701f55a83022a1a12999130edea54832737aa81e
mindikaev <aidarmindikaev@gmail.com>
2021-12-12 14:25:01 +0500

    Создан проект внутри локальной копии репозитория, подключен к проекту Selenium и составлен небольшой работающий тест, который просто запускает браузер, открывает страницу и закрывает браузер.

---
+++ selenium-training/MyFirstTest/src/test/java/MyFirstTest.java
@@ -0,0 +1,37 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start () {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait (driver, 20);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://www.bbc.com/");
+        driver.findElement(By.id("orb-search-q")).sendKeys("England");
+        driver.findElement(By.id("orb-search-button")).click();
+        WebElement searchLink = driver.findElement(By.cssSelector("main#main-content li:first-child"));
+            searchLink.click();
+
+    }
+
+    @After
+    public void stop () {
+        driver.quit();
+        driver = null;
+    }
+}
+
