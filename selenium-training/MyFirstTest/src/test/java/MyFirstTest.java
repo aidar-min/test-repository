9711634564f676891aa445c26552e10289efe13f
mindikaev <aidarmindikaev@gmail.com>
2021-12-21 19:33:36 +0500

    Сделан сценарий, который выполняет следующие действия в учебном приложении litecart.
    1) входит в панель администратора http://localhost/litecart/admin
    2) прокликивает последовательно все пункты меню слева, включая вложенные пункты
    3) для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)

--- selenium-training/MyFirstTest/src/test/java/MyFirstTest.java
+++ selenium-training/MyFirstTest/src/test/java/MyFirstTest.java
@@ -15,7 +15,7 @@ public class MyFirstTest {
     @Before
     public void start () {
         driver = new ChromeDriver();
-        wait = new WebDriverWait (driver, 20);
+        wait = new WebDriverWait (driver, 10);
     }
 
     @Test
