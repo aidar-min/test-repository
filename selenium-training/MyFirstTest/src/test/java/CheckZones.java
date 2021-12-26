2bb626050628d9644cc47ffee7adedd8c776be72
mindikaev <aidarmindikaev@gmail.com>
2021-12-26 21:08:18 +0500

    Откорректировал сортировку.

--- selenium-training/MyFirstTest/src/test/java/CheckZones.java
+++ selenium-training/MyFirstTest/src/test/java/CheckZones.java
@@ -20,9 +20,9 @@ public class CheckZones {
     @Before
     public void start() {
         driver = new ChromeDriver();
-        //ждем 40 секунд, чтобы найти элемент
-        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 40);
+        //ждем 20 секунд, чтобы найти элемент
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 20);
 
     }
 
@@ -34,29 +34,35 @@ public class CheckZones {
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
 
-        for (int i = 2; i <= driver.findElements(By.cssSelector("tr.row")).size() + 1; i++) {
-            driver.findElement(By.cssSelector("tr:nth-child(" + i + ") > td:nth-child(3)>a")).click();
+        List<WebElement> geoZonesList = driver.findElements(By.xpath("//tr[@class='row']//a[contains(@href,'geo_zone_id')and not (contains(@title,'Edit'))]"));
 
-            List<WebElement> listZones = driver.findElements(By.cssSelector("td:nth-child(3)>select[name^='zones']"));
-            List<String> SortZones = new ArrayList<>();
-            List<String> Zones = new ArrayList<>();
+        for (int k = 0; k < geoZonesList.size(); k++) {
+            int geoIndex = k + 1;
+            WebElement geoZone = driver.findElement(By.xpath("//tr[@class='row'][" + geoIndex + "]//a[contains(@href,'geo_zone_id')and not (contains(@title,'Edit'))]"));
+            geoZone.click();
 
-            for (WebElement listZona : listZones) {
+            List<WebElement> geoZonesZonesList = driver.findElements(By.xpath("//table[@id='table-zones']//select[contains(@name,'zone_code')]//option[@selected='selected']"));
+            ArrayList<String> getGeoZonesList = new ArrayList();
+            ArrayList<String> sortGeoZonesList = new ArrayList();
 
-                if (listZona.isSelected() == true) {
-                    String textZona = (String) listZona.getAttribute("text");
-                    SortZones.add((String) textZona);
-                    Zones.add((String) textZona);
-                }
-
+            for (WebElement geoZonesZones : geoZonesZonesList) {
+                String geoZonesName = geoZonesZones.getText();
+                getGeoZonesList.add(geoZonesName);
             }
-            if (SortZones.size() != 0) {
-                Collections.sort(SortZones);
-                if (SortZones.equals(Zones) == true) {
-                    System.out.println("sorting is correct");
-                }
+            System.out.println("getGeoZonesList = " + getGeoZonesList);
+            for (String getGeoZone : getGeoZonesList) {
+                sortGeoZonesList.add(getGeoZone);
             }
-            driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
+
+            Collections.sort(sortGeoZonesList);
+            System.out.println("sortGeoZonesList = " + sortGeoZonesList);
+
+            if (sortGeoZonesList.equals(getGeoZonesList)) {
+                System.out.println("SUCCESS: Список отсортирован в алфавитном порядке");
+            } else {
+                System.out.println("ERROR: Список не в алфавитном порядке");
+            }
+            driver.navigate().back();
         }
 
     }
